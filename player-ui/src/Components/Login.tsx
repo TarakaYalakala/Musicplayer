7b5d32ffb11c7e8a5830575b64b1a8b85ae9928c
unknown <tarakayalakala@gmail.com>
2025-03-08 14:40:11 +0530

    DeletedUnwanted

--- player-ui/src/Components/Login.tsx
+++ player-ui/src/Components/Login.tsx
@@ -4,14 +4,17 @@ function Login() {
   return (
     <div className='flex items-center justify-center bg-blue-800 h-160'>
     <div className='login-container bg-black text-white font-sans h-120 w-120 flex justify-center items-center'>
-        <h1>Login....</h1>
-        <div className="boxes">
-          <div className="icon">
-          <i class="fa-brands fa-spotify text-white"></i>
+        <div className="boxes bg-amber-900 p-10">
+          {/* icon */}
+          <div className="icon font-5xl font-bold text-[25px] bg-blue-800 flex justify-center items-center p-5">
+         <i className="fa-brands fa-spotify fa-2xl"></i>
           </div>
-          
+          <div className="context font-bold text-[30px] text-white font-sans">
+            Login in to Spotify
+          </div>
             {/* One */}
             <div className="boxone">
+              
                 
 
 
